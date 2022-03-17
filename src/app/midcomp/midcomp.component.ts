f788b641aa5b72ab3f0c3f1e57c46a92e2ae257d
Afrah Riyaz <afrahriyaz@gmail.com>
2022-03-18 00:24:33 +0530

    midcomp created

---
+++ src/app/midcomp/midcomp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-midcomp',
+  templateUrl: './midcomp.component.html',
+  styleUrls: ['./midcomp.component.css']
+})
+export class MidcompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
