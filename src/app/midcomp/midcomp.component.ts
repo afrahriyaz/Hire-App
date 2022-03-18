6960da87e1218da91109a6a24c6dbfa1ebbc877f
Afrah Riyaz <afrahriyaz@gmail.com>
2022-03-18 18:15:52 +0530

    upcoming interviews data fetched by calling api

--- src/app/midcomp/midcomp.component.ts
+++ src/app/midcomp/midcomp.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ScheduleService } from 'src/app/services/schedule.service';
 @Component({
   selector: 'app-midcomp',
   templateUrl: './midcomp.component.html',
   styleUrls: ['./midcomp.component.css']
 })
 export class MidcompComponent implements OnInit {
-
-  constructor() { }
+  schedule:any;
+  constructor(private data:ScheduleService)
+  {
+    data.schedule().subscribe((a)=>{
+      console.warn("data",a);
+      this.schedule=a
+    })
+  }
 
   ngOnInit(): void {
   }
